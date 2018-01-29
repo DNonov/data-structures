910639747b41a0ed93b9e284d9e0f504c9795905
Dimitar Nonov <D.Nonov@gmail.com>
2018-01-29 14:56:56 +0000

    licensing list file

--- testov.js
+++ testov.js
@@ -4,4 +4,11 @@ list.append('Pesho');
 list.append('Bylgar');
 list.append('Unufri');
 
+list.next();
+list.next();
+console.log(list.getElement());
+list.head();
+console.log(list.getElement());
+
+
 console.log(list.display());
