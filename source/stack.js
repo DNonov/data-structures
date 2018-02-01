359ad54f588b2d196209ab287d19c45361574a2f
Dimitar Nonov <D.Nonov@gmail.com>
2018-02-01 22:22:33 +0000

    stack added

---
+++ source/stack.js
@@ -0,0 +1,19 @@
+function stack() {
+  this.container = [];
+  this.top = 0;
+  this.push = push;
+  this.pop = pop;
+  this.peek = peek;
+}
+
+function push(element) {
+  
+}
+
+function pop() {
+  
+}
+
+function peek() {
+  
+}
