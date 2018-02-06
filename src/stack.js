26789418450caf4793ac2904c04ece703aa97a5d
Dimitar Nonov <D.Nonov@gmail.com>
2018-02-06 16:58:02 +0000

    description added to the stack function constructor

--- src/stack.js
+++ src/stack.js
@@ -25,6 +25,14 @@
 /**
  * Function constructor of a stack.
  * 
+ * @description In computer science, a stack is an abstract data type that serves as a 
+ * collection of elements, with two principal operations:
+ * push, which adds an element to the collection, and
+ * pop, which removes the most recently added element that was not yet removed.
+ * The order in which elements come off a stack gives rise to its alternative name, 
+ * LIFO (last in, first out). Additionally, a peek operation may give access to the top without modifying the stack.
+ * Full wikipedia article at: https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
+ * 
  */
 function Stack() {
   this._container = [];
