3b13b2d2c311e58f49cd6dd1fc84f5a0c86455c1
Dimitar Nonov <D.Nonov@gmail.com>
2018-02-15 01:21:58 +0000

    gulp watch task added

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 const gulp = require('gulp');
+const watch = require('gulp-watch');
 const jasmine = require('gulp-jasmine');
 
-gulp.task('test', function () {
+gulp.task('test', () => {
   return gulp.src('test/**/*.test.js')
     .pipe(jasmine());
+});
+
+gulp.task('watch-test', () => {
+  watch('test/**/*.test.js', () => {
+    gulp.start('test');
+  });
 });
