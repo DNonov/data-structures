9add13f9772b982dbe824a34b4e2263bed566b86
Dimitar Nonov <D.Nonov@gmail.com>
2018-02-22 21:21:22 +0000

    gulp ES6 again

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,18 @@
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const jasmine = require('gulp-jasmine');
 
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var jasmine = require('gulp-jasmine');
-
-gulp.task('test', function () {
+gulp.task('test', () => {
   return gulp.src('test/**/*.test.js')
     .pipe(jasmine());
 });
 
-gulp.task('watch-test', function () {
+gulp.task('watch-test',() => {
   watch('test/**/*.test.js', function () {
     gulp.start('test');
   });
 });
 
-gulp.task('build', function () {
+gulp.task('build', () => {
   gulp.start('test');
 });
