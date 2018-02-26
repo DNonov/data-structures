b84752d04f48ef1f8e45a39f25a5f236e16c3855
Dimitar Nonov <D.Nonov@gmail.com>
2018-02-26 22:49:42 +0000

    index.js file added

---
+++ index.js
@@ -0,0 +1,11 @@
+(function () {
+  return module.exports = {
+    BST:        require("./src/binarySearchTree"),
+    Dictionary: require("./src/dictionary"),
+    LinkedList: require("./src/linkedList"),
+    List:       require("./src/list"),
+    Queue:      require("./src/queue"),
+    _Set:       require("./src/set"),
+    Stack:      require("./src/stack")
+  }
+})();
