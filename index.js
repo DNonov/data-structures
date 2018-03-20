df55818c9eec641ccf518100e3cd101439da3def
Dimitar Nonov <d.nonov@gmail.com>
2018-03-20 23:18:03 +0000

    Add the heaps to index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 (function () {
   return module.exports = {
-    BST:        require("./src/binarySearchTree"),
-    Dictionary: require("./src/dictionary"),
-    LinkedList: require("./src/linkedList"),
-    List:       require("./src/list"),
-    Queue:      require("./src/queue"),
-    _Set:       require("./src/set"),
-    Stack:      require("./src/stack")
+    BST:              require("./src/binarySearchTree"),
+    Dictionary:       require("./src/dictionary"),
+    DoublyLinkedList: require("./src/doublyLinkedList"),
+    LinkedList:       require("./src/linkedList"),
+    List:             require("./src/list"),
+    MinHeap:          require("./src/minHeap"),
+    MaxHeap:          require("./src/maxHeap"),
+    Queue:            require("./src/queue"),
+    _Set:             require("./src/set"),
+    Stack:            require("./src/stack")
   }
 })();
