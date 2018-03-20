44d6b071d943a141dbb022919ba60233ed76b057
Dimitar Nonov <d.nonov@gmail.com>
2018-03-20 20:42:35 +0000

    Prefix container with underscore, to 'mark'it as private

--- src/maxHeap.js
+++ src/maxHeap.js
@@ -1,5 +1,5 @@
 function MaxHeap () {
-  const container = [null];
+  const _container = [null];
 
   // Node function constructor
   function Node(element, priority) {
@@ -9,42 +9,53 @@ function MaxHeap () {
 
   this.insert = function (element, priority) {
     const newNode = new Node (element, priority);
-    container.push(newNode);
-    let currNodeIndex = container.length - 1;
+    _container.push(newNode);
+    let currNodeIndex = _container.length - 1;
     let parentNodeIndex = Math.floor(currNodeIndex / 2);
 
-    while (container[parentNodeIndex] && newNode.priority > container[parentNodeIndex].priority) {
-      const parent = container[parentNodeIndex];
-      container[parentNodeIndex] = newNode;
-      container[currNodeIndex] = parent;
+    while (_container[parentNodeIndex] && newNode.priority > _container[parentNodeIndex].priority) {
+      const parent = _container[parentNodeIndex];
+      _container[parentNodeIndex] = newNode;
+      _container[currNodeIndex] = parent;
       currNodeIndex = parentNodeIndex;
       parentNodeIndex = Math.floor(currNodeIndex / 2);
     }
   }
 
   this.remove = function () {
-    if (container.length < 3) { 
-      const toReturn = container.pop();
-      container[0] = null;
+    if (_container.length < 3) { 
+      const toReturn = _container.pop();
+      _container[0] = null;
       return toReturn;
     }
 
-    const toRemove = container[1];
-    container[1] = container.pop();
+    const toRemove = _container[1];
+    _container[1] = _container.pop();
     let currIndex = 1;
     let [left, right] = [2 * currIndex, 2 * currIndex + 1];
-    let currChildIndex = container[right] && container[right].priority >= container[left].priority ? right : left;
-    while (container[currChildIndex] && container[currIndex].priority <= container[currChildIndex].priority) {
-      let currNode = container[currIndex];
-      let currChildNode = container[currChildIndex];
-      container[currChildIndex] = currNode;
-      container[currIndex] = currChildNode;
+    let currChildIndex = _container[right] && _container[right].priority >= _container[left].priority ? right : left;
+    while (_container[currChildIndex] && _container[currIndex].priority <= _container[currChildIndex].priority) {
+      let currNode = _container[currIndex];
+      let currChildNode = _container[currChildIndex];
+      _container[currChildIndex] = currNode;
+      _container[currIndex] = currChildNode;
     }
     return toRemove;
   }
 
   this.peek = function () {
-    return container[1].element;
+    return _container[1].element;
+  }
+
+  this.isEmpty = function () {
+    if (_container[1] !== null) {
+      return false;
+    }
+    return true;
+  }
+
+  this.toArray = function () {
+    return _container;
   }
 }
 module.exports = MaxHeap;
