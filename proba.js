3cbed9f46c38d1df8fc346557b028955c92b8708
Dimitar Nonov <d.nonov@gmail.com>
2018-03-20 16:20:33 +0000

    Implement MaxHeap

--- proba.js
+++ proba.js
@@ -1,4 +1,4 @@
-const Heap = require('./src/minHeap');
+const Heap = require('./src/maxHeap');
 const myHeap = new Heap();
 
 myHeap.insert('Cat',22);
@@ -6,6 +6,6 @@ myHeap.insert('Bull',8);
 myHeap.insert('Ass',3);
 myHeap.insert('Ass',12);
 myHeap.insert('Ass',5);
-myHeap.insert('Ass',67);
+myHeap.insert('Lol',67);
 
-console.log(myHeap.display());
+console.log(myHeap.peek());
