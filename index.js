b6b0c3f40369c8248fb095a55fa9cb8cea6e5cb5
Dimitar Nonov <d.nonov@gmail.com>
2019-05-30 22:38:42 +0100

    Remove List and change double quote to single

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 (function () {
   return module.exports = {
-    BST:              require("./src/binarySearchTree"),
-    Dictionary:       require("./src/dictionary"),
-    DoublyLinkedList: require("./src/doublyLinkedList"),
-    LinkedList:       require("./src/linkedList"),
-    List:             require("./src/list"),
-    MaxHeap:          require("./src/maxHeap"),
-    MinHeap:          require("./src/minHeap"),
-    PriorityQueueMax: require("./src/priorityQueueMax"),
-    PriorityQueueMin: require("./src/priorityQueueMin"),
-    Queue:            require("./src/queue"),
-    _Set:             require("./src/set"),
-    Stack:            require("./src/stack")
-  }
+    BST:              require('./src/binarySearchTree'),
+    Dictionary:       require('./src/dictionary'),
+    DoublyLinkedList: require('./src/doublyLinkedList'),
+    LinkedList:       require('./src/linkedList'),
+    MaxHeap:          require('./src/maxHeap'),
+    MinHeap:          require('./src/minHeap'),
+    PriorityQueueMax: require('./src/priorityQueueMax'),
+    PriorityQueueMin: require('./src/priorityQueueMin'),
+    Queue:            require('./src/queue'),
+    _Set:             require('./src/set'),
+    Stack:            require('./src/stack')
+  };
 })();
