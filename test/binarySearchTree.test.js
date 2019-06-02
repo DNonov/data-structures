3f385cc9bfe9bd24cb57cf3cad8e1a309458b9b1
Dimitar Nonov <d.nonov@gmail.com>
2019-06-02 21:03:13 +0100

    Tidy up binarySearchTree.test.js

--- test/binarySearchTree.test.js
+++ test/binarySearchTree.test.js
@@ -1,46 +1,41 @@
-const BST        = require('../src/binarySearchTree');
-const jasmine    = require('jasmine');
+const BST = require('../src/binarySearchTree');
 
 describe('BST method', () => {
-  it('insearts new node', () => {
-    let myBST = new BST();
-    myBST.insert(1);
-    expect(myBST.inOrder()).toEqual([1]);
-    myBST.insert(2);
-    expect(myBST.inOrder()).toEqual([1, 2]);
-    myBST.insert('cat');
-    expect(myBST.inOrder()).toEqual([1, 2, 'cat']);
+  let myBST;
+
+  beforeEach(() => {
+    myBST = new BST();
   });
+
   it('finds the smallest value', () => {
-    let myBST = new BST();
-    myBST.insert(47); 
+    myBST.insert(47);
     myBST.insert(23);
     myBST.insert(2);
     myBST.insert(4);
     myBST.insert(17);
     expect(myBST.min()).toEqual(2);
   });
+
   it('finds the biggest value', () => {
-    let myBST = new BST();
     myBST.insert(47);
     myBST.insert(23);
     myBST.insert(2);
     myBST.insert(4);
-    myBST.insert(17);  
+    myBST.insert(17);
     expect(myBST.max()).toEqual(47);
   });
+
   it('finds given value', () => {
-    let myBST = new BST();
     myBST.insert(47);
     myBST.insert(23);
     myBST.insert(2);
     myBST.insert(4);
-    myBST.insert(17);  
+    myBST.insert(17);
     expect(myBST.find(2)).toEqual(2);
     expect(myBST.find(23)).toEqual(23);
   });
+
   it('removes given node', () => {
-    let myBST = new BST();
     expect(myBST.remove(23)).toBe(undefined);
     expect(myBST.remove()).toBe(false);
     myBST.insert(47);
@@ -49,7 +44,7 @@ describe('BST method', () => {
     myBST.insert(2);
     myBST.insert(4);
     myBST.insert(17);
-    myBST.remove(2);  
+    myBST.remove(2);
     expect(myBST.inOrder()).toEqual([4, 17, 23, 23, 47]);
     myBST.remove(23);
     expect(myBST.inOrder()).toEqual([4, 17, 23, 47]);
