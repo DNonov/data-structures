27f838a9556d23bc9c61089f751a57bee1bce3b7
Dimitar Nonov <d.nonov@gmail.com>
2019-06-02 23:52:08 +0100

    Tidy up queue.test.js

--- test/queue.test.js
+++ test/queue.test.js
@@ -1,18 +1,20 @@
 const Queue = require('../src/queue');
-const jasmine = require('jasmine');
 
 describe('Queue method', () => {
+  let testQueue;
+
+  beforeEach(() => {
+    testQueue = new Queue();
+  });
+
   it('adds element in a queue', () => {
-    let testQueue = new Queue();
     testQueue.enqueue(1);
     testQueue.enqueue(2);
     testQueue.enqueue(3);
     expect(testQueue.toArray()).toEqual([1, 2, 3]);
-    
   });
-  
+
   it('removes and returns element in a queue', () => {
-    let testQueue = new Queue();
     expect(testQueue.dequeue()).toBe(false);
     testQueue.enqueue(1);
     testQueue.enqueue(2);
@@ -22,18 +24,16 @@ describe('Queue method', () => {
     expect(testQueue.dequeue()).toBe(3);
     expect(testQueue.isEmpty()).toBe(true);
   });
-  
+
   it('returns the first element in a queue', () => {
-    let testQueue = new Queue();
     expect(testQueue.head()).toBe(false);
     testQueue.enqueue(1);
     testQueue.enqueue(2);
     testQueue.enqueue(3);
     expect(testQueue.head()).toBe(1);
   });
-  
+
   it('returns the last element in a queue', () => {
-    let testQueue = new Queue();
     expect(testQueue.tail()).toBe(false);
     testQueue.enqueue(1);
     testQueue.enqueue(2);
@@ -42,7 +42,6 @@ describe('Queue method', () => {
   });
 
   it('returns true if queue is empty', () => {
-    let testQueue = new Queue();
     expect(testQueue.isEmpty()).toBe(true);
     testQueue.enqueue(1);
     expect(testQueue.isEmpty()).toBe(false);
