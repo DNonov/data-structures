2c756432542522bca0384455d01cec2074087c18
Dimitar Nonov <d.nonov@gmail.com>
2019-06-03 23:24:03 +0100

    Tidy up priorityQueueMax.test.js

--- test/priorityQueueMax.test.js
+++ test/priorityQueueMax.test.js
@@ -1,9 +1,13 @@
 const PriorityQueue = require('../src/priorityQueueMax');
-const jasmine = require('jasmine');
 
 describe('PriorityQueueMax method', () => {
+  let myPriorityQueue;
+
+  beforeEach(() => {
+    myPriorityQueue = new PriorityQueue();
+  });
+
   it('insert element in a PriorityQueue', () => {
-    const myPriorityQueue = new PriorityQueue();
     expect(myPriorityQueue.enqueue()).toEqual(false);
     myPriorityQueue.enqueue('Rat');
     expect(myPriorityQueue.enqueue('Rat', 'fish')).toEqual(undefined);
@@ -14,28 +18,25 @@ describe('PriorityQueueMax method', () => {
     myPriorityQueue.enqueue('Bat', 2);
     expect(myPriorityQueue.head()).toBe('Dog');
   });
-  
+
   it('removes and returns element in a PriorityQueue', () => {
-    const myPriorityQueue = new PriorityQueue();
-    myPriorityQueue.enqueue('Rat'); 
+    myPriorityQueue.enqueue('Rat');
     myPriorityQueue.enqueue('Cat', 1);
     myPriorityQueue.enqueue('Dog', 21);
     myPriorityQueue.enqueue('Bat', 2);
-    myPriorityQueue.dequeue(); 
+    myPriorityQueue.dequeue();
     expect(myPriorityQueue.head()).toBe('Bat');
     myPriorityQueue.enqueue('Pig', 31);
     expect(myPriorityQueue.head()).toBe('Pig');
-  }); 
-  
+  });
+
   it('returns true if a PriorityQueue is empty', () => {
-    let myPriorityQueue = new PriorityQueue();
     expect(myPriorityQueue.isEmpty()).toBe(true);
     myPriorityQueue.enqueue('Bat', 2);
     expect(myPriorityQueue.isEmpty()).toBe(false);
   });
-  
+
   it('returns array representation of a PriorityQueue', () => {
-    let myPriorityQueue = new PriorityQueue();
     expect(myPriorityQueue.toArray()).toEqual([]);
     expect(myPriorityQueue.head()).toEqual(false);
     myPriorityQueue.enqueue('Cat', 1);
@@ -45,4 +46,4 @@ describe('PriorityQueueMax method', () => {
     expect(myPriorityQueue.toArray()).toEqual(['Dog', 'Cat', 'Bat']);
     expect(myPriorityQueue.head()).toEqual('Dog');
   });
-}); 
+});
