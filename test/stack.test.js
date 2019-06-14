3460a23a3fef956b5eba27e85c7bbe5be5563088
Dimitar Nonov <d.nonov@gmail.com>
2019-06-14 23:46:22 +0100

    Add more test to stack's push and length methods

--- test/stack.test.js
+++ test/stack.test.js
@@ -11,8 +11,18 @@ describe('Stack method', () => {
     expect(() => stack.push()).toThrow(new Error('undefined or missing argument'));
   });
 
-  it('pushes an element into a stack', () => {
+  it('returns 0 on empty stack while length is called', () => {
+    expect(stack.length()).toEqual(0);
+  });
 
+  it('returns the length of a stack', () => {
+    stack.push(1);
+    expect(stack.length()).toEqual(1);
+  });
+
+  it('pushes element into a stack', () => {
+    stack.push(1);
+    expect(stack.length()).toEqual(1);
   });
 
   it('removes the top element in a stack', () => {
@@ -36,17 +46,5 @@ describe('Stack method', () => {
     expect(stack.peek()).toBe(4);
     stack.clear();
     expect(stack.peek()).toBe(false);
-  });
-
-  it('returns the length of a stack', () => {
-    expect(stack.length()).toBe(0);
-    stack.push(1);
-    expect(stack.length()).toBe(1);
-    stack.push(2);
-    expect(stack.length()).toBe(2);
-    stack.push(3);
-    expect(stack.length()).toBe(3);
-    stack.push(4);
-    expect(stack.length()).toBe(4);
   });
 });
